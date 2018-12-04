cd154c898e394b30e0bc2bfb62b6f519c69bbd42
Moh. Khoirul Arifin <karifin998@gmail.com>
2018-12-04 14:05:49 +0700

    PPP

--- src/test/MainGUI.java
+++ src/test/MainGUI.java
@@ -3,5 +3,7 @@ public class MainGUI {
     public static void main(String[] args) {
         String Nama;
         // TODO code application logic here
+        System.out.println("Hello Word!!");
+                
     }
 }
