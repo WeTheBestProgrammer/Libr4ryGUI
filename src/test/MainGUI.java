f2475ac62c4f7a440c87428280824a42b82f1b36
ahmadmusyadada <ahmadmusyadadaminullah@gmail.com>
2018-12-04 14:00:36 +0700

    Update MainGUI.java

--- src/test/MainGUI.java
+++ src/test/MainGUI.java
@@ -1,17 +1,7 @@
 package test;
-
-/**
- *
- * @author Ahmad Musyadad A
- */
 public class MainGUI {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        private String Nama;
+        String Nama;
         // TODO code application logic here
     }
-    
 }
