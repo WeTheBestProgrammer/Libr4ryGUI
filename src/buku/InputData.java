cf096a7b49b23c808cd1887afdf0e8f8842e4a62
Ahmad Musyadad Aminullah <43369569+ahmadmusyadada@users.noreply.github.com>
2018-12-15 18:37:27 +0700

    Troubleshoot

--- src/buku/InputData.java
+++ src/buku/InputData.java
@@ -1,18 +1,20 @@
 package buku;
 import java.util.ArrayList;
 public class InputData {
-    ArrayList<Mahasiswa> ListMahasiswa;
+    ArrayList<Item> ListMahasiswa;
 
     public InputData() {
         ListMahasiswa = new ArrayList();
     }
 
-    public void isiData(String nim, String nama, String alamat){
-        Mahasiswa mhs = new Mahasiswa(nim, nama, alamat);
+    public void isiData(int code, String nama, String kategori, String judul,
+                        int lamaPinjam, String tanggalPinjam, String tanggalKembali,
+                        int harga, int biaya){
+        Item mhs = new Item(code, nama, kategori, judul, lamaPinjam, tanggalPinjam, tanggalKembali, harga, biaya);
         ListMahasiswa.add(mhs);
     }
 
-    public ArrayList<Mahasiswa> getData() {
+    public ArrayList<Item> getData() {
         return ListMahasiswa;
     }
 }
