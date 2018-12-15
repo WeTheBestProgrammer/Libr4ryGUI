ef45ee3ea7d2e461f4fe5f4705e8bb9564f7abbe
ahmadmusyadada <ahmadmusyadadaminullah@gmail.com>
2018-12-15 17:59:00 +0700

    Revert "Big Update"
    This reverts commit 1516b92444ae2f88b55df1d4d303d71b023da357.

--- src/buku/InputData.java
+++ src/buku/InputData.java
@@ -1,20 +1,18 @@
 package buku;
 import java.util.ArrayList;
 public class InputData {
-    ArrayList<Item> ListMahasiswa;
+    ArrayList<Mahasiswa> ListMahasiswa;
 
     public InputData() {
         ListMahasiswa = new ArrayList();
     }
 
-    public void isiData(int code, String nama, String kategori, String judul,
-                        int lamaPinjam, String tanggalPinjam, String tanggalKembali,
-                        int harga, int biaya){
-        Item mhs = new Item(code, nama, kategori, judul, lamaPinjam, tanggalPinjam, tanggalKembali, harga, biaya);
+    public void isiData(String nim, String nama, String alamat){
+        Mahasiswa mhs = new Mahasiswa(nim, nama, alamat);
         ListMahasiswa.add(mhs);
     }
 
-    public ArrayList<Item> getData() {
+    public ArrayList<Mahasiswa> getData() {
         return ListMahasiswa;
     }
 }
--- src/buku/Item.java
+++ src/buku/Item.java
@@ -4,57 +4,36 @@
  * and open the template in the editor.
  */
 package buku;
+
 /**
  *
  * @author Ahmad Musyadad A
  */
 public class Item {
-    int codePinjam, lamaPinjam, harga, biaya;
-    String namaMahasiswa, kategoriBuku, judulBuku, tanggalPinjam, tanggalKembali;
+    String jenisBuku;
+    String judulBuku;
+    int harga;
 
-    public Item(int codePinjam, String namaMahasiswa, String kategoriBuku, String judulBuku,
-                int lamaPinjam, String tanggalPinjam, String tanggalKembali, int harga, int biaya) {
-        this.codePinjam = codePinjam;
-        this.lamaPinjam = lamaPinjam;
-        this.tanggalPinjam = tanggalPinjam;
-        this.tanggalKembali = tanggalKembali;
-        this.harga = harga;
-        this.biaya = biaya;
-        this.namaMahasiswa = namaMahasiswa;
-        this.kategoriBuku = kategoriBuku;
+    public Item(String jenisBuku, String judulBuku, int harga) {
+        this.jenisBuku = jenisBuku;
         this.judulBuku = judulBuku;
+        this.harga = harga;
     }
 
-    public int getCodePinjam() {
-        return codePinjam;
+    public String getJenisBuku() {
+        return jenisBuku;
     }
 
-    public void setCodePinjam(int codePinjam) {
-        this.codePinjam = codePinjam;
+    public void setJenisBuku(String jenisBuku) {
+        this.jenisBuku = jenisBuku;
     }
 
-    public int getLamaPinjam() {
-        return lamaPinjam;
+    public String getJudulBuku() {
+        return judulBuku;
     }
 
-    public void setLamaPinjam(int lamaPinjam) {
-        this.lamaPinjam = lamaPinjam;
-    }
-
-    public String getTanggalPinjam() {
-        return tanggalPinjam;
-    }
-
-    public void setTanggalPinjam(String tanggalPinjam) {
-        this.tanggalPinjam = tanggalPinjam;
-    }
-
-    public String getTanggalKembali() {
-        return tanggalKembali;
-    }
-
-    public void setTanggalKembali(String tanggalKembali) {
-        this.tanggalKembali = tanggalKembali;
+    public void setJudulBuku(String judulBuku) {
+        this.judulBuku = judulBuku;
     }
 
     public int getHarga() {
@@ -65,41 +44,8 @@ public class Item {
         this.harga = harga;
     }
 
-    public int getBiaya() {
-        return biaya;
-    }
-
-    public void setBiaya(int biaya) {
-        this.biaya = biaya;
-    }
-
-    public String getNamaMahasiswa() {
-        return namaMahasiswa;
-    }
-
-    public void setNamaMahasiswa(String namaMahasiswa) {
-        this.namaMahasiswa = namaMahasiswa;
-    }
-
-    public String getKategoriBuku() {
-        return kategoriBuku;
-    }
-
-    public void setKategoriBuku(String kategoriBuku) {
-        this.kategoriBuku = kategoriBuku;
-    }
-
-
-    public String getJudulBuku() {
-        return judulBuku;
-    }
-
-    public void setJudulBuku(String judulBuku) {
-        this.judulBuku = judulBuku;
-    }
-
     @Override
     public String toString() {
-        return this.kategoriBuku;
+        return this.jenisBuku;
     }
 }
