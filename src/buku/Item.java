8cbe73833b17786f0e3fac5dbebecb83fd1e2982
ahmadmusyadada <ahmadmusyadadaminullah@gmail.com>
2018-12-15 18:10:05 +0700

    Error

--- src/buku/Item.java
+++ src/buku/Item.java
@@ -4,36 +4,57 @@
  * and open the template in the editor.
  */
 package buku;
-
 /**
  *
  * @author Ahmad Musyadad A
  */
 public class Item {
-    String jenisBuku;
-    String judulBuku;
-    int harga;
+    int codePinjam, lamaPinjam, harga, biaya;
+    String namaMahasiswa, kategoriBuku, judulBuku, tanggalPinjam, tanggalKembali;
 
-    public Item(String jenisBuku, String judulBuku, int harga) {
-        this.jenisBuku = jenisBuku;
-        this.judulBuku = judulBuku;
+    public Item(int codePinjam, String namaMahasiswa, String kategoriBuku, String judulBuku,
+                int lamaPinjam, String tanggalPinjam, String tanggalKembali, int harga, int biaya) {
+        this.codePinjam = codePinjam;
+        this.lamaPinjam = lamaPinjam;
+        this.tanggalPinjam = tanggalPinjam;
+        this.tanggalKembali = tanggalKembali;
         this.harga = harga;
-    }
-
-    public String getJenisBuku() {
-        return jenisBuku;
-    }
-
-    public void setJenisBuku(String jenisBuku) {
-        this.jenisBuku = jenisBuku;
-    }
-
-    public String getJudulBuku() {
-        return judulBuku;
-    }
-
-    public void setJudulBuku(String judulBuku) {
+        this.biaya = biaya;
+        this.namaMahasiswa = namaMahasiswa;
+        this.kategoriBuku = kategoriBuku;
         this.judulBuku = judulBuku;
+    }
+
+    public int getCodePinjam() {
+        return codePinjam;
+    }
+
+    public void setCodePinjam(int codePinjam) {
+        this.codePinjam = codePinjam;
+    }
+
+    public int getLamaPinjam() {
+        return lamaPinjam;
+    }
+
+    public void setLamaPinjam(int lamaPinjam) {
+        this.lamaPinjam = lamaPinjam;
+    }
+
+    public String getTanggalPinjam() {
+        return tanggalPinjam;
+    }
+
+    public void setTanggalPinjam(String tanggalPinjam) {
+        this.tanggalPinjam = tanggalPinjam;
+    }
+
+    public String getTanggalKembali() {
+        return tanggalKembali;
+    }
+
+    public void setTanggalKembali(String tanggalKembali) {
+        this.tanggalKembali = tanggalKembali;
     }
 
     public int getHarga() {
@@ -44,8 +65,41 @@ public class Item {
         this.harga = harga;
     }
 
+    public int getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(int biaya) {
+        this.biaya = biaya;
+    }
+
+    public String getNamaMahasiswa() {
+        return namaMahasiswa;
+    }
+
+    public void setNamaMahasiswa(String namaMahasiswa) {
+        this.namaMahasiswa = namaMahasiswa;
+    }
+
+    public String getKategoriBuku() {
+        return kategoriBuku;
+    }
+
+    public void setKategoriBuku(String kategoriBuku) {
+        this.kategoriBuku = kategoriBuku;
+    }
+
+
+    public String getJudulBuku() {
+        return judulBuku;
+    }
+
+    public void setJudulBuku(String judulBuku) {
+        this.judulBuku = judulBuku;
+    }
+
     @Override
     public String toString() {
-        return this.jenisBuku;
+        return this.kategoriBuku;
     }
 }
