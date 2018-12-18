f78038583b25590dde5dc2f37b28292e84ee422f
Moh. Khoirul Arifin <karifin998@gmail.com>
2018-12-18 19:02:27 +0700

    Update DatabaeConnector

--- src/database/DatabaseConnector.java
+++ src/database/DatabaseConnector.java
@@ -12,16 +12,41 @@ import java.sql.*;
  * @author KINTUL
  */
 public class DatabaseConnector {
-    private Connection databaseConnection = null;
-
-    public DatabaseConnector(String url, String user, String password) throws SQLException {
-        databaseConnection = DriverManager.getConnection("jdbc:mysql://localhost/perpustakaan", "root", "");
+    private static Connection koneksi;
+    int harga = 0;
+    
+    private static void buka_koneksi(){
+        if (koneksi == null) {
+            try {
+//                String url = "jdbc:mysql://localhost/perpustakaan";
+                String url = "jdbc:mysql://192.168.100.4:3306/perpustakaan";
+                String user = "root";
+                String password = "pass";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+            } catch (SQLException t) {
+//                System.out.println("Error membuat koneksi");
+                System.err.print(t);
+            }
+        }
     }
     
-    public void close() throws SQLException {
-        databaseConnection.close();
+    public void setHarga(String kategori, String judul){
+        buka_koneksi();
+        ResultSet rs = null;
+        
+        String sql = "SELECT harga_sat from buku";
+                    
+        try {
+            PreparedStatement mStatement = koneksi.prepareStatement(sql);
+            Statement state = koneksi.createStatement();
+            rs =  state.executeQuery("select harga_sat from buku where kategori = '" + kategori + "' and judul ='" + judul + "'");
+            while (rs.next()) {                
+                harga = rs.getInt("harga_sat");
+            }
+                mStatement.close();
+        } catch (Exception l) {
+            System.err.println(l);
+        }
     }
-
-    
-    
 }
